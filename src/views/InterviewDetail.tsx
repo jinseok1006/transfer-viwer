6ffd00cb8c94c94f5f5e1ccb86bc65b4f5326562
jinseok1006 <jinseok1006@naver.com>
2024-07-28 17:02:06 +0900

    update

--- src/views/InterviewDetail.tsx
+++ src/views/InterviewDetail.tsx
@@ -11,30 +11,32 @@ import NoInterviewPost from "../components/NoInterviewPost";
 import Head from "../components/Head";
 // import Error from "../components/ErrorBoudary";
 import Loading from "../components/Loading";
-import { useDepartmentLinkStore, useDivisionsStore } from "../store/transferStatistics";
+import {
+  useDepartmentLinkStore,
+  useDivisionsStore,
+} from "../store/transferStatistics";
 import { extractApiAttribues } from "../utils/util";
 import staticDataApi from "../api/staticData";
 import { useEffect } from "react";
 
-
 export default function InterviewInfoPage() {
-  const departmentLinks= useDepartmentLinkStore();
-  
+  const departmentLinks = useDepartmentLinkStore();
+
   const divisions = useDivisionsStore((state) => state.divisions);
   const [searchParams] = useSearchParams();
   const division = searchParams.get("division");
 
-  useEffect(()=> {
-    if(!departmentLinks.data) {
+  useEffect(() => {
+    if (!departmentLinks.data) {
       departmentLinks.fetchData();
     }
   }, []);
-  
+
   if (departmentLinks.loading) {
     return <Loading />;
   }
   if (departmentLinks.error || !departmentLinks.data) {
-    throw new Error('departmentLinks 데이터 로딩 실패');
+    throw new Error("departmentLinks 데이터 로딩 실패");
   }
 
   if (!division) return <NotFound />;
@@ -83,70 +85,54 @@ function InterviewInfoContainer({ division }: { division: string }) {
   );
 }
 
-function InterviewInfo({ division : department }: { division: string }) {
-
-
-//   const departmentLinksState = useAsync(getDepartmentLinks);
-//   const departmentLinks = departmentLinksState.value;
-//   const relatedDepartments = departmentLinks[division];
-//   const { loading, error, value } = useAsync(
-//     () => fetchInterviewPostByDepartments(relatedDepartments),
-//     [relatedDepartments]
-//   );
-
-
-//   if(departmentLinksState.error || !departmentLinksState.value ) {
-//     return <Error error={departmentLinksState.error} />;
-//   }
-
-//   if (departmentLinksState.loading) {
-//     return <Loading />;
-//   }
-
-//   // const departmentLinks = departmentLinksState.value;
-//   // const relatedDepartments = departmentLinks[division];
-
-
-
-
-//   if (error || !value) {
-//     return <Error error={error} />;
-//   }
-//   if (loading) {
-//     return <Loading />;
-//   }
-
-//   const interviewPosts = extractApiAttribues(value);
-
-//   // console.log('success');
-
-//   if (interviewPosts.length === 0) {
-//     return <NoInterviewPost />;
-//   }
-
-
-
-//   return (
-//     <>
-//       <Stack direction="column" spacing={5}>
-//         {interviewPosts.map(
-//           (
-//             { department, year, isYearPrivate, grade, score,  hasTakenCourse, content },
-//             i
-//           ) => (
-//             <InterviewPostCard
-//               key={i}
-//               department={department}
-//               isYearPrivate={isYearPrivate}
-//               year={year}
-//               grade={grade}
-//               score={score}
-//               hasTakenCourse={hasTakenCourse}
-//               content={content}
-//             />
-//           )
-//         )}
-//       </Stack>
-//     </>
-//   );
+function InterviewInfo({ division: department }: { division: string }) {
+  //   const departmentLinksState = useAsync(getDepartmentLinks);
+  //   const departmentLinks = departmentLinksState.value;
+  //   const relatedDepartments = departmentLinks[division];
+  //   const { loading, error, value } = useAsync(
+  //     () => fetchInterviewPostByDepartments(relatedDepartments),
+  //     [relatedDepartments]
+  //   );
+  //   if(departmentLinksState.error || !departmentLinksState.value ) {
+  //     return <Error error={departmentLinksState.error} />;
+  //   }
+  //   if (departmentLinksState.loading) {
+  //     return <Loading />;
+  //   }
+  //   // const departmentLinks = departmentLinksState.value;
+  //   // const relatedDepartments = departmentLinks[division];
+  //   if (error || !value) {
+  //     return <Error error={error} />;
+  //   }
+  //   if (loading) {
+  //     return <Loading />;
+  //   }
+  //   const interviewPosts = extractApiAttribues(value);
+  //   // console.log('success');
+  //   if (interviewPosts.length === 0) {
+  //     return <NoInterviewPost />;
+  //   }
+  //   return (
+  //     <>
+  //       <Stack direction="column" spacing={5}>
+  //         {interviewPosts.map(
+  //           (
+  //             { department, year, isYearPrivate, grade, score,  hasTakenCourse, content },
+  //             i
+  //           ) => (
+  //             <InterviewPostCard
+  //               key={i}
+  //               department={department}
+  //               isYearPrivate={isYearPrivate}
+  //               year={year}
+  //               grade={grade}
+  //               score={score}
+  //               hasTakenCourse={hasTakenCourse}
+  //               content={content}
+  //             />
+  //           )
+  //         )}
+  //       </Stack>
+  //     </>
+  //   );
 }
